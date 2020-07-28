61b9d1eaccef8c60959ce60c68405521c30b2aba
Prajakta Mane <prajakta916mane1@gmail.com>
2020-07-28 09:48:10 +0530

    java

---
+++ April Challenge/day1Singlenumber.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int x = nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            x = x^nums[i];
+        }
+        return x;
+    }
+}
